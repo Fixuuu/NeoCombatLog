99d0e5a9ff0760c029ff2f70ed81d1c9860a9323
kukubaczek <kukubaczek@gmail.com>
2016-03-12 15:00:45 +0100

    Wstępne listenery, podstawowa komenda
    Dodano:
    - KickListener,
    - QuitListener,
    - CommandWalka.
    
    Poprawiono:
    - Main.

--- src/net/kukubaczek/neocombatlog/Main.java
+++ src/net/kukubaczek/neocombatlog/Main.java
@@ -5,16 +5,44 @@ import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import net.kukubaczek.neocombatlog.listeners.*;
+import net.kukubaczek.neocombatlog.walka.CommandWalka;
 
 public class Main extends JavaPlugin {
 	
+	public static int logoutTime = 20;
+	public static String version;
+	
+	public static boolean deathKick = true;
+	public static boolean deathLogout = true;
+	
 	public void onEnable(){
+		log("-=- Rozpoczynanie ladowania... -=-");
+		
+		log("Ladowanie konfiguracji...");
+        this.getConfig().options().copyDefaults(true);
+        this.saveConfig();
+		
+        version = this.getDescription().getVersion();
+        
+		log("Ladowanie listenerów...");
 		PluginManager pm = Bukkit.getPluginManager();
 		pm.registerEvents(new LoginListener(), this);
+        
+		if(deathLogout) pm.registerEvents(new QuitListener(), this);
+		if(deathKick) pm.registerEvents(new KickListener(), this);
+		
+		log("Ladowanie komend...");
+		getCommand("walka").setExecutor(new CommandWalka());
+		
+		log("-=- Dziekujemy za korzystanie z pluginów grupy Neo -=-");
 	}
 	
 	public void onDisable(){
 		
 	}
 
+	public void log(String msg){
+		System.out.print("[NeoCombatLog] " + msg);
+	}
+	
 }
---
+++ src/net/kukubaczek/neocombatlog/listeners/KickListener.java
@@ -0,0 +1,14 @@
+package net.kukubaczek.neocombatlog.listeners;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerKickEvent;
+
+public class KickListener implements Listener{
+	
+	@EventHandler
+	public void kick(PlayerKickEvent e){
+		
+	}
+
+}
